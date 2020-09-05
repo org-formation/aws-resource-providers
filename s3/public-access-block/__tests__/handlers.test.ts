64ef29f5b598a57b7674dffcfa3713ac3d9797e7
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2020-09-05 23:56:38 +0200

    update tests for s3 access block

--- s3/public-access-block/__tests__/handlers.test.ts
+++ s3/public-access-block/__tests__/handlers.test.ts
@@ -1,12 +1,20 @@
 import { S3Control } from 'aws-sdk';
 import { on, AwsServiceMockBuilder } from '@jurijzahn8019/aws-promise-jest-mock';
-import { Action, exceptions, SessionProxy, UnmodeledRequest } from 'cfn-rpdk';
+import {
+    Action,
+    exceptions,
+    OperationStatus,
+    SessionProxy,
+    UnmodeledRequest,
+} from 'cfn-rpdk';
 import createFixture from '../sam-tests/create.json';
 import deleteFixture from '../sam-tests/delete.json';
 import readFixture from '../sam-tests/read.json';
 import updateFixture from '../sam-tests/update.json';
 import { resource } from '../src/handlers';
 import { ResourceModel } from '../src/models';
+
+const IDENTIFIER = '123456789012';
 
 jest.mock('aws-sdk');
 
@@ -26,6 +34,13 @@ describe('when calling handler', () => {
 
     beforeEach(async () => {
         s3control = on(S3Control, { snapshot: false });
+        s3control.mock('getPublicAccessBlock').resolve({
+            PublicAccessBlockConfiguration: {
+                BlockPublicAcls: true,
+            },
+        });
+        s3control.mock('putPublicAccessBlock').resolve({});
+        s3control.mock('deletePublicAccessBlock').resolve({});
         session['client'] = () => s3control.instance;
     });
 
@@ -33,6 +48,80 @@ describe('when calling handler', () => {
         jest.clearAllMocks();
         jest.restoreAllMocks();
     });
+
+    test('create operation successful', async () => {
+        const request = UnmodeledRequest.fromUnmodeled(
+            fixtureMap.get(Action.Create)
+        ).toModeled<ResourceModel>(resource['modelCls']);
+        const progress = await resource['invokeHandler'](
+            session,
+            request,
+            Action.Create,
+            {}
+        );
+        const model = request.desiredResourceState;
+        model.resourceId = IDENTIFIER;
+        expect(progress.serialize()).toMatchObject({
+            status: OperationStatus.Success,
+            message: '',
+            callbackDelaySeconds: 0,
+            resourceModel: model.serialize(),
+        });
+    });
+
+    test('update operation successful', async () => {
+        const request = UnmodeledRequest.fromUnmodeled(
+            fixtureMap.get(Action.Update)
+        ).toModeled<ResourceModel>(resource['modelCls']);
+        const progress = await resource['invokeHandler'](
+            session,
+            request,
+            Action.Update,
+            {}
+        );
+        expect(progress).toMatchObject({
+            status: OperationStatus.Success,
+            message: '',
+            callbackDelaySeconds: 0,
+            resourceModel: request.desiredResourceState,
+        });
+    });
+
+    test('delete operation successful', async () => {
+        const request = UnmodeledRequest.fromUnmodeled(
+            fixtureMap.get(Action.Delete)
+        ).toModeled<ResourceModel>(resource['modelCls']);
+        const progress = await resource['invokeHandler'](
+            session,
+            request,
+            Action.Delete,
+            {}
+        );
+        expect(progress).toMatchObject({
+            status: OperationStatus.Success,
+            message: '',
+            callbackDelaySeconds: 0,
+        });
+        expect(progress.resourceModel).toBeNull();
+    });
+
+    // test('read operation successful', async () => {
+    //     const request = UnmodeledRequest.fromUnmodeled(
+    //         fixtureMap.get(Action.Read)
+    //     ).toModeled<ResourceModel>(resource['modelCls']);
+    //     const progress = await resource['invokeHandler'](
+    //         session,
+    //         request,
+    //         Action.Read,
+    //         {}
+    //     );
+    //     expect(progress).toMatchObject({
+    //         status: OperationStatus.Success,
+    //         message: '',
+    //         callbackDelaySeconds: 0,
+    //         resourceModel: request.desiredResourceState,
+    //     });
+    // });
 
     test('all operations fail without session', async () => {
         const promises: any[] = [];
