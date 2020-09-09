4dd263da7022d6dd86b06653a9d42ac7c3cc4a52
Olaf Conijn <olaf_conijn@hotmail.com>
2020-09-09 23:10:55 +0200

    updated service quota s3 resource provider to use common decorator

--- service-quotas/s3/__tests__/handlers.test.ts
+++ service-quotas/s3/__tests__/handlers.test.ts
@@ -1,32 +1,49 @@
-import { S3Control } from 'aws-sdk';
+import { ServiceQuotas } from 'aws-sdk';
 import { on, AwsServiceMockBuilder } from '@jurijzahn8019/aws-promise-jest-mock';
-import { Action, exceptions, SessionProxy, UnmodeledRequest } from 'cfn-rpdk';
+import {
+    Action,
+    exceptions,
+    SessionProxy,
+    UnmodeledRequest,
+    OperationStatus,
+} from 'cfn-rpdk';
 import createFixture from '../sam-tests/create.json';
-import deleteFixture from '../sam-tests/delete.json';
-import readFixture from '../sam-tests/read.json';
+import updateDecreaseBucketsFixture from '../sam-tests/update-decrease-buckets.json';
 import updateFixture from '../sam-tests/update.json';
 import { resource } from '../src/handlers';
 import { ResourceModel } from '../src/models';
 
 jest.mock('aws-sdk');
 
+const IDENTIFIER = '123456789012';
+
 describe('when calling handler', () => {
     let session: SessionProxy;
-    let s3control: AwsServiceMockBuilder<S3Control>;
+    let serviceQuotas: AwsServiceMockBuilder<ServiceQuotas>;
+    let listRequestedServiceQuotaChangeHistoryByQuotaMock: any;
+    let getServiceQuotaMock: any;
+    let requestServiceQuotaIncreaseMock: any;
     let fixtureMap: Map<Action, Record<string, any>>;
 
     beforeAll(() => {
         session = new SessionProxy({});
         fixtureMap = new Map<Action, Record<string, any>>();
         fixtureMap.set(Action.Create, createFixture);
-        fixtureMap.set(Action.Read, readFixture);
+        //fixtureMap.set(Action.Read, readFixture);
         fixtureMap.set(Action.Update, updateFixture);
-        fixtureMap.set(Action.Delete, deleteFixture);
+        //fixtureMap.set(Action.Delete, deleteFixture);
     });
 
     beforeEach(async () => {
-        s3control = on(S3Control, { snapshot: false });
-        session['client'] = () => s3control.instance;
+        serviceQuotas = on(ServiceQuotas, { snapshot: false });
+        listRequestedServiceQuotaChangeHistoryByQuotaMock = serviceQuotas
+            .mock('listRequestedServiceQuotaChangeHistoryByQuota')
+            .resolve({});
+        getServiceQuotaMock = serviceQuotas.mock('getServiceQuota').resolve({});
+        requestServiceQuotaIncreaseMock = serviceQuotas
+            .mock('requestServiceQuotaIncrease')
+            .resolve({});
+        session['client'] = () => serviceQuotas.instance;
     });
 
     afterEach(() => {
@@ -50,5 +67,98 @@ describe('when calling handler', () => {
         });
         expect.assertions(promises.length);
         await Promise.all(promises);
+    });
+
+    test('service quotas s3 create operation successful', async () => {
+        const request = UnmodeledRequest.fromUnmodeled(
+            fixtureMap.get(Action.Create)
+        ).toModeled<ResourceModel>(resource['modelCls']);
+        const progress = await resource['invokeHandler'](
+            session,
+            request,
+            Action.Create,
+            {}
+        );
+        const model = request.desiredResourceState;
+        model.resourceId = IDENTIFIER;
+        expect(progress.serialize()).toMatchObject({
+            status: OperationStatus.Success,
+            message: '',
+            callbackDelaySeconds: 0,
+            resourceModel: model.serialize(),
+        });
+
+        expect(listRequestedServiceQuotaChangeHistoryByQuotaMock.mock).toBeCalledTimes(
+            1
+        );
+        expect(
+            listRequestedServiceQuotaChangeHistoryByQuotaMock.mock
+        ).toHaveBeenCalledWith({ QuotaCode: 'L-DC2B2D3D', ServiceCode: 's3' });
+        expect(getServiceQuotaMock.mock).toBeCalledTimes(1);
+        expect(getServiceQuotaMock.mock).toHaveBeenCalledWith({
+            QuotaCode: 'L-DC2B2D3D',
+            ServiceCode: 's3',
+        });
+        expect(requestServiceQuotaIncreaseMock.mock).toBeCalledTimes(1);
+        expect(requestServiceQuotaIncreaseMock.mock).toHaveBeenCalledWith({
+            QuotaCode: 'L-DC2B2D3D',
+            ServiceCode: 's3',
+            DesiredValue: 100,
+        });
+    });
+
+    test('service quotas s3 update operation successful', async () => {
+        const request = UnmodeledRequest.fromUnmodeled(
+            fixtureMap.get(Action.Update)
+        ).toModeled<ResourceModel>(resource['modelCls']);
+        const progress = await resource['invokeHandler'](
+            session,
+            request,
+            Action.Update,
+            {}
+        );
+        expect(progress).toMatchObject({
+            status: OperationStatus.Success,
+            message: '',
+            callbackDelaySeconds: 0,
+            resourceModel: request.desiredResourceState,
+        });
+
+        expect(listRequestedServiceQuotaChangeHistoryByQuotaMock.mock).toBeCalledTimes(
+            1
+        );
+        expect(
+            listRequestedServiceQuotaChangeHistoryByQuotaMock.mock
+        ).toHaveBeenCalledWith({ QuotaCode: 'L-DC2B2D3D', ServiceCode: 's3' });
+        expect(getServiceQuotaMock.mock).toBeCalledTimes(1);
+        expect(getServiceQuotaMock.mock).toHaveBeenCalledWith({
+            QuotaCode: 'L-DC2B2D3D',
+            ServiceCode: 's3',
+        });
+        expect(requestServiceQuotaIncreaseMock.mock).toBeCalledTimes(1);
+        expect(requestServiceQuotaIncreaseMock.mock).toHaveBeenCalledWith({
+            QuotaCode: 'L-DC2B2D3D',
+            ServiceCode: 's3',
+            DesiredValue: 110,
+        });
+    });
+
+    test('service quotas s3 update decrease bucket limit fails', async () => {
+        const updateDecreaseBuckets = updateDecreaseBucketsFixture;
+        const request = UnmodeledRequest.fromUnmodeled(updateDecreaseBuckets).toModeled<
+            ResourceModel
+        >(resource['modelCls']);
+
+        expect(async () => {
+            await resource['invokeHandler'](session, request, Action.Update, {});
+        }).rejects.toThrowError(
+            /Decrease of limit failed because desired value 90 is lower than previous value 100/
+        );
+
+        expect(listRequestedServiceQuotaChangeHistoryByQuotaMock.mock).toBeCalledTimes(
+            0
+        );
+        expect(getServiceQuotaMock.mock).toBeCalledTimes(0);
+        expect(requestServiceQuotaIncreaseMock.mock).toBeCalledTimes(0);
     });
 });
--- service-quotas/s3/src/handlers.ts
+++ service-quotas/s3/src/handlers.ts
@@ -1,12 +1,7 @@
 import { Action, BaseResource, exceptions, handlerEvent } from 'cfn-rpdk';
 import { ResourceModel } from './models';
 import { ServiceQuotas } from 'aws-sdk';
-import {
-    commonAws,
-    UpsertQuotas,
-    QuotaID,
-    HandlerArgs,
-} from 'aws-resource-providers-common';
+import { commonAws, UpsertQuotas, QuotaID, HandlerArgs } from 'aws-resource-providers-common';
 
 // Use this logger to forward log messages to CloudWatch Logs.
 const LOGGER = console;
@@ -14,48 +9,27 @@ const LOGGER = console;
 const quotaCodeForPropertyName: Record<string, QuotaID> = {
     buckets: { QuotaCode: 'L-DC2B2D3D', ServiceCode: 's3' },
 };
+
 class Resource extends BaseResource<ResourceModel> {
     @handlerEvent(Action.Create)
     @commonAws({ serviceName: 'ServiceQuotas' })
-    public async create(
-        action: Action,
-        args: HandlerArgs<ResourceModel>,
-        service: ServiceQuotas
-    ): Promise<ResourceModel> {
+    public async create(action: Action, args: HandlerArgs<ResourceModel>, service: ServiceQuotas): Promise<ResourceModel> {
         const model = args.request.desiredResourceState;
         const accountId = args.request.awsAccountId;
         model.resourceId = accountId;
 
-        await UpsertQuotas(
-            service,
-            new ResourceModel(),
-            model,
-            quotaCodeForPropertyName,
-            LOGGER
-        );
+        await UpsertQuotas(service, new ResourceModel(), model, quotaCodeForPropertyName, LOGGER);
 
         return model;
     }
 
     @handlerEvent(Action.Update)
     @commonAws({ serviceName: 'ServiceQuotas' })
-    public async update(
-        action: Action,
-        args: HandlerArgs<ResourceModel>,
-        service: ServiceQuotas
-    ): Promise<ResourceModel> {
+    public async update(action: Action, args: HandlerArgs<ResourceModel>, service: ServiceQuotas): Promise<ResourceModel> {
         const model = args.request.desiredResourceState;
         const previousModel = args.request.previousResourceState;
-        const accountId = args.request.awsAccountId;
-        model.resourceId = accountId;
 
-        await UpsertQuotas(
-            service,
-            previousModel,
-            model,
-            quotaCodeForPropertyName,
-            LOGGER
-        );
+        await UpsertQuotas(service, previousModel, model, quotaCodeForPropertyName, LOGGER);
 
         return model;
     }
