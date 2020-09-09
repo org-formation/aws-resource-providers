43197ec67b36a0cdb46cd8624114a77295c01ce6
Olaf Conijn <olaf_conijn@hotmail.com>
2020-09-09 23:16:19 +0200

    linting

--- service-quotas/s3/src/handlers.ts
+++ service-quotas/s3/src/handlers.ts
@@ -1,7 +1,12 @@
 import { Action, BaseResource, exceptions, handlerEvent } from 'cfn-rpdk';
 import { ResourceModel } from './models';
 import { ServiceQuotas } from 'aws-sdk';
-import { commonAws, UpsertQuotas, QuotaID, HandlerArgs } from 'aws-resource-providers-common';
+import {
+    commonAws,
+    UpsertQuotas,
+    QuotaID,
+    HandlerArgs,
+} from 'aws-resource-providers-common';
 
 // Use this logger to forward log messages to CloudWatch Logs.
 const LOGGER = console;
@@ -13,23 +18,43 @@ const quotaCodeForPropertyName: Record<string, QuotaID> = {
 class Resource extends BaseResource<ResourceModel> {
     @handlerEvent(Action.Create)
     @commonAws({ serviceName: 'ServiceQuotas' })
-    public async create(action: Action, args: HandlerArgs<ResourceModel>, service: ServiceQuotas): Promise<ResourceModel> {
+    public async create(
+        action: Action,
+        args: HandlerArgs<ResourceModel>,
+        service: ServiceQuotas
+    ): Promise<ResourceModel> {
         const model = args.request.desiredResourceState;
         const accountId = args.request.awsAccountId;
         model.resourceId = accountId;
 
-        await UpsertQuotas(service, new ResourceModel(), model, quotaCodeForPropertyName, LOGGER);
+        await UpsertQuotas(
+            service,
+            new ResourceModel(),
+            model,
+            quotaCodeForPropertyName,
+            LOGGER
+        );
 
         return model;
     }
 
     @handlerEvent(Action.Update)
     @commonAws({ serviceName: 'ServiceQuotas' })
-    public async update(action: Action, args: HandlerArgs<ResourceModel>, service: ServiceQuotas): Promise<ResourceModel> {
+    public async update(
+        action: Action,
+        args: HandlerArgs<ResourceModel>,
+        service: ServiceQuotas
+    ): Promise<ResourceModel> {
         const model = args.request.desiredResourceState;
         const previousModel = args.request.previousResourceState;
 
-        await UpsertQuotas(service, previousModel, model, quotaCodeForPropertyName, LOGGER);
+        await UpsertQuotas(
+            service,
+            previousModel,
+            model,
+            quotaCodeForPropertyName,
+            LOGGER
+        );
 
         return model;
     }
