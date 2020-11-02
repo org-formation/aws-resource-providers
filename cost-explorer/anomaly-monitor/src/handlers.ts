77ed2f9b86775c954f96a5fc18a69cda49818acc
Olaf Conijn <olaf_conijn@hotmail.com>
2020-11-02 12:41:00 +0100

    chore(cost-explorer): linting

--- cost-explorer/anomaly-monitor/src/handlers.ts
+++ cost-explorer/anomaly-monitor/src/handlers.ts
@@ -3,26 +3,21 @@ import { commonAws, HandlerArgs } from 'aws-resource-providers-common';
 import { ResourceModel } from './models';
 import { CostExplorer } from 'aws-sdk';
 
-// Use this logger to forward log messages to CloudWatch Logs.
-const LOGGER = console;
-
-interface CallbackContext extends Record<string, any> {}
-
 class Resource extends BaseResource<ResourceModel> {
-
     @handlerEvent(Action.Create)
     @commonAws({ serviceName: 'CostExplorer', debug: true })
     public async create(action: Action, args: HandlerArgs<any>, service: CostExplorer, model: ResourceModel): Promise<ResourceModel> {
-
-        const response = await service.createAnomalyMonitor({
-            AnomalyMonitor: {
-                MonitorName: model.monitorName,
-                MonitorDimension: model.monitorDimension,
-                MonitorType: model.monitorType,
-                MonitorSpecification: model.monitorSpecification as CostExplorer.Expression,
-                DimensionalValueCount: model.dimensionalValueCount
-            }
-        }).promise();
+        const response = await service
+            .createAnomalyMonitor({
+                AnomalyMonitor: {
+                    MonitorName: model.monitorName,
+                    MonitorDimension: model.monitorDimension,
+                    MonitorType: model.monitorType,
+                    MonitorSpecification: model.monitorSpecification as CostExplorer.Expression,
+                    DimensionalValueCount: model.dimensionalValueCount,
+                },
+            })
+            .promise();
 
         model.arn = response.MonitorArn;
         return Promise.resolve(model);
@@ -33,11 +28,10 @@ class Resource extends BaseResource<ResourceModel> {
     public async delete(action: Action, args: HandlerArgs<any>, service: CostExplorer): Promise<ResourceModel> {
         const model: ResourceModel = args.request.desiredResourceState;
 
-        await service.deleteAnomalyMonitor( { MonitorArn: model.arn}).promise();
+        await service.deleteAnomalyMonitor({ MonitorArn: model.arn }).promise();
 
         return Promise.resolve(null);
     }
-
 }
 
 const resource = new Resource(ResourceModel.TYPE_NAME, ResourceModel);
--- cost-explorer/anomaly-subscription/src/handlers.ts
+++ cost-explorer/anomaly-subscription/src/handlers.ts
@@ -4,27 +4,22 @@ import { ResourceModel } from './models';
 import { CostExplorer } from 'aws-sdk';
 import { Subscribers } from 'aws-sdk/clients/costexplorer';
 
-// Use this logger to forward log messages to CloudWatch Logs.
-const LOGGER = console;
-
-interface CallbackContext extends Record<string, any> {}
-
 class Resource extends BaseResource<ResourceModel> {
-
     @handlerEvent(Action.Create)
     @commonAws({ serviceName: 'CostExplorer', debug: true })
     public async create(action: Action, args: HandlerArgs<any>, service: CostExplorer, model: ResourceModel): Promise<ResourceModel> {
-
-        const response = await service.createAnomalySubscription({
-            AnomalySubscription: {
-                AccountId: model.accountId,
-                MonitorArnList: model.monitorArnList,
-                Subscribers: model.subscribers as Subscribers,
-                Threshold: model.threshold,
-                Frequency: model.frequency,
-                SubscriptionName: model.subscriptionName
-            }
-        }).promise();
+        const response = await service
+            .createAnomalySubscription({
+                AnomalySubscription: {
+                    AccountId: model.accountId,
+                    MonitorArnList: model.monitorArnList,
+                    Subscribers: model.subscribers as Subscribers,
+                    Threshold: model.threshold,
+                    Frequency: model.frequency,
+                    SubscriptionName: model.subscriptionName,
+                },
+            })
+            .promise();
 
         model.arn = response.SubscriptionArn;
         return Promise.resolve(model);
@@ -35,7 +30,7 @@ class Resource extends BaseResource<ResourceModel> {
     public async delete(action: Action, args: HandlerArgs<any>, service: CostExplorer): Promise<ResourceModel> {
         const model: ResourceModel = args.request.desiredResourceState;
 
-        await service.deleteAnomalySubscription( { SubscriptionArn: model.arn}).promise();
+        await service.deleteAnomalySubscription({ SubscriptionArn: model.arn }).promise();
 
         return Promise.resolve(null);
     }
