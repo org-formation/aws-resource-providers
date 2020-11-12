fce3e1a73b665ebb75a9554bace9026478e476b0
Olaf Conijn <olaf_conijn@hotmail.com>
2020-11-12 12:57:46 +0100

    chore(security-hub): linting

--- security-hub/master/src/handlers.ts
+++ security-hub/master/src/handlers.ts
@@ -9,21 +9,18 @@ const LOGGER = console;
 const versionCode = '1';
 
 interface LogContext {
-    handler: 'Create' | 'Update' | 'Delete',
+    handler: 'Create' | 'Update' | 'Delete';
     versionCode: string;
     clientRequestToken: string;
 }
 
-interface CallbackContext extends Record<string, any> {}
-
 async function acceptMaster(desiredModel: ResourceModel, loggingContext: LogContext, service: SecurityHub) {
-
     LOGGER.info({ ...loggingContext, method: 'acceptMaster', desiredModel });
 
     const invitations = await service.listInvitations().promise();
     LOGGER.info({ ...loggingContext, method: 'after listInvitations', invitations });
 
-    for(const invitation of invitations.Invitations) {
+    for (const invitation of invitations.Invitations) {
         if (invitation.AccountId === desiredModel.masterAccountId) {
             const acceptInvitationRequest: AcceptInvitationRequest = { InvitationId: invitation.InvitationId, MasterId: desiredModel.masterAccountId };
 
@@ -34,7 +31,6 @@ async function acceptMaster(desiredModel: ResourceModel, loggingContext: LogCont
     }
 
     LOGGER.info({ ...loggingContext, method: 'acceptMaster', desiredModel });
-
 }
 
 class Resource extends BaseResource<ResourceModel> {
@@ -42,7 +38,7 @@ class Resource extends BaseResource<ResourceModel> {
     @commonAws({ serviceName: 'SecurityHub', debug: true })
     public async create(action: Action, args: HandlerArgs<ResourceModel>, service: SecurityHub): Promise<ResourceModel> {
         const { desiredResourceState, awsAccountId, clientRequestToken } = args.request;
-        const loggingContext : LogContext = { handler: 'Create', clientRequestToken: clientRequestToken, versionCode };
+        const loggingContext: LogContext = { handler: 'Create', clientRequestToken: clientRequestToken, versionCode };
 
         LOGGER.info({ ...loggingContext, message: 'begin', args });
         await acceptMaster(desiredResourceState, loggingContext, service);
@@ -57,7 +53,7 @@ class Resource extends BaseResource<ResourceModel> {
     @commonAws({ serviceName: 'SecurityHub', debug: true })
     public async update(action: Action, args: HandlerArgs<ResourceModel>, service: SecurityHub): Promise<ResourceModel> {
         const { desiredResourceState, clientRequestToken } = args.request;
-        const loggingContext : LogContext = { handler: 'Update', clientRequestToken: clientRequestToken, versionCode };
+        const loggingContext: LogContext = { handler: 'Update', clientRequestToken: clientRequestToken, versionCode };
 
         LOGGER.info({ ...loggingContext, message: 'begin', args });
         await acceptMaster(desiredResourceState, loggingContext, service);
@@ -68,9 +64,9 @@ class Resource extends BaseResource<ResourceModel> {
 
     @handlerEvent(Action.Delete)
     @commonAws({ serviceName: 'SecurityHub', debug: true })
-    public async delete(action: Action, args: HandlerArgs<ResourceModel>, service: SecurityHub): Promise<ResourceModel> {
+    public async delete(action: Action, args: HandlerArgs<ResourceModel> /*, service: SecurityHub*/): Promise<ResourceModel> {
         const { desiredResourceState, clientRequestToken } = args.request;
-        const loggingContext : LogContext = { handler: 'Delete', clientRequestToken: clientRequestToken, versionCode };
+        const loggingContext: LogContext = { handler: 'Delete', clientRequestToken: clientRequestToken, versionCode };
 
         LOGGER.info({ ...loggingContext, message: 'noop', args });
 
--- security-hub/members/src/handlers.ts
+++ security-hub/members/src/handlers.ts
@@ -2,7 +2,6 @@ import { SecurityHub } from 'aws-sdk';
 import { commonAws, HandlerArgs } from 'aws-resource-providers-common';
 import { ResourceModel } from './models';
 import { BaseResource, handlerEvent, Action } from 'cfn-rpdk';
-import { CreateMembersRequest, InviteMembersRequest } from 'aws-sdk/clients/securityhub';
 import { InternalFailure } from 'cfn-rpdk/dist/exceptions';
 
 // Use this logger to forward log messages to CloudWatch Logs.
@@ -10,45 +9,41 @@ const LOGGER = console;
 const versionCode = '1';
 
 interface LogContext {
-    handler: 'Create' | 'Update' | 'Delete',
+    handler: 'Create' | 'Update' | 'Delete';
     versionCode: string;
     clientRequestToken: string;
 }
 
 async function inviteMembers(desiredModel: ResourceModel, loggingContext: LogContext, service: SecurityHub) {
-
     LOGGER.info({ ...loggingContext, method: 'inviteMembers', desiredModel });
 
     if (desiredModel.memberAccountIDs === undefined) {
         throw new InternalFailure(`memberAccountIds are undefined`);
     }
-    const accountDetails = desiredModel.memberAccountIDs.map(x => ({ AccountId: x }));
+    const accountDetails = desiredModel.memberAccountIDs.map((x) => ({ AccountId: x }));
 
     const createRequest = {
-        AccountDetails: accountDetails
+        AccountDetails: accountDetails,
     };
 
     LOGGER.info({ ...loggingContext, method: 'before createMembers', desiredModel, createRequest });
     const createResponse = await service.createMembers(createRequest).promise();
     LOGGER.info({ ...loggingContext, method: 'after createMembers', desiredModel, createRequest, createResponse });
 
-    
     const inviteRequest = {
-        AccountIds: desiredModel.memberAccountIDs
+        AccountIds: desiredModel.memberAccountIDs,
     };
     LOGGER.info({ ...loggingContext, method: 'before inviteMembers', desiredModel, inviteRequest });
     const inviteResponse = await service.inviteMembers(inviteRequest).promise();
     LOGGER.info({ ...loggingContext, method: 'after inviteMembers', desiredModel, inviteRequest, inviteResponse });
 }
 
-interface CallbackContext extends Record<string, any> {}
-
 class Resource extends BaseResource<ResourceModel> {
     @handlerEvent(Action.Create)
     @commonAws({ serviceName: 'SecurityHub', debug: true })
     public async create(action: Action, args: HandlerArgs<ResourceModel>, service: SecurityHub): Promise<ResourceModel> {
         const { desiredResourceState, awsAccountId, clientRequestToken } = args.request;
-        const loggingContext : LogContext = { handler: 'Create', clientRequestToken: clientRequestToken, versionCode };
+        const loggingContext: LogContext = { handler: 'Create', clientRequestToken: clientRequestToken, versionCode };
 
         LOGGER.info({ ...loggingContext, message: 'begin', args });
         await inviteMembers(desiredResourceState, loggingContext, service);
@@ -63,7 +58,7 @@ class Resource extends BaseResource<ResourceModel> {
     @commonAws({ serviceName: 'SecurityHub', debug: true })
     public async update(action: Action, args: HandlerArgs<ResourceModel>, service: SecurityHub): Promise<ResourceModel> {
         const { desiredResourceState, clientRequestToken } = args.request;
-        const loggingContext : LogContext = { handler: 'Update', clientRequestToken: clientRequestToken, versionCode };
+        const loggingContext: LogContext = { handler: 'Update', clientRequestToken: clientRequestToken, versionCode };
 
         LOGGER.info({ ...loggingContext, message: 'begin', args });
 
@@ -75,9 +70,9 @@ class Resource extends BaseResource<ResourceModel> {
 
     @handlerEvent(Action.Delete)
     @commonAws({ serviceName: 'SecurityHub', debug: true })
-    public async delete(action: Action, args: HandlerArgs<ResourceModel>, service: SecurityHub): Promise<ResourceModel> {
+    public async delete(action: Action, args: HandlerArgs<ResourceModel>): Promise<ResourceModel> {
         const { desiredResourceState, clientRequestToken } = args.request;
-        const loggingContext : LogContext = { handler: 'Delete', clientRequestToken: clientRequestToken, versionCode };
+        const loggingContext: LogContext = { handler: 'Delete', clientRequestToken: clientRequestToken, versionCode };
 
         LOGGER.info({ ...loggingContext, message: 'noop', args });
 
@@ -90,4 +85,3 @@ const resource = new Resource(ResourceModel.TYPE_NAME, ResourceModel);
 export const entrypoint = resource.entrypoint;
 
 export const testEntrypoint = resource.testEntrypoint;
-
