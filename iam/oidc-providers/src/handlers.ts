b6f3c09fd82656f73e5225fd7a397ce55ba71ac8
Olaf Conijn <olaf_conijn@hotmail.com>
2020-11-12 10:31:31 +0100

    fix(oidc-provider): fix permission issue, added possibility to change list of client ids

--- iam/oidc-providers/src/handlers.ts
+++ iam/oidc-providers/src/handlers.ts
@@ -1,14 +1,14 @@
-import { Action, BaseResource, exceptions, handlerEvent } from 'cfn-rpdk';
+import { Action, BaseResource, exceptions, handlerEvent, Logger } from 'cfn-rpdk';
 import { ResourceModel } from './models';
 import { IAM } from 'aws-sdk';
 import { commonAws, HandlerArgs } from 'aws-resource-providers-common';
+import { AddClientIDToOpenIDConnectProviderRequest, RemoveClientIDFromOpenIDConnectProviderRequest, UpdateOpenIDConnectProviderThumbprintRequest } from 'aws-sdk/clients/iam';
 
-class Resource extends BaseResource<ResourceModel> {
+export class Resource extends BaseResource<ResourceModel> {
     @handlerEvent(Action.Create)
     @commonAws({ serviceName: 'IAM', debug: true })
-    public async create(action: Action, args: HandlerArgs<ResourceModel>, service: IAM, model: ResourceModel): Promise<ResourceModel> {
-        const response = await service
-            .createOpenIDConnectProvider({
+    public async create(action: Action, args: HandlerArgs<ResourceModel>, service: IAM, model: ResourceModel, logger: Logger): Promise<ResourceModel> {
+        const response = await service.createOpenIDConnectProvider({
                 ClientIDList: model.clientIDList,
                 ThumbprintList: model.thumbprintList,
                 Url: model.url,
@@ -21,31 +21,44 @@ class Resource extends BaseResource<ResourceModel> {
 
     @handlerEvent(Action.Update)
     @commonAws({ serviceName: 'IAM', debug: true })
-    public async update(action: Action, args: HandlerArgs<ResourceModel>, service: IAM, model: ResourceModel): Promise<ResourceModel> {
+    public async update(action: Action, args: HandlerArgs<ResourceModel>, service: IAM, model: ResourceModel, logger: Logger): Promise<ResourceModel> {
         const previousModel = args.request.previousResourceState;
 
         if (previousModel.url !== model.url) {
             throw new exceptions.InvalidRequest(`Changing the url of an oidc provider is not supported.`);
         }
 
-        const prevClientIds = (previousModel.clientIDList ?? []).sort();
-        const clientIds = (model.clientIDList ?? []).sort();
+        const prevClientIds = (previousModel.clientIDList ?? []);
+        const clientIds = (model.clientIDList ?? []);
+        const clientIdsNeedToAdd = clientIds.filter(x=>!prevClientIds.includes(x));
+        const clientIdsNeedToRemove = prevClientIds.filter(x=>!clientIds.includes(x));
 
-        if (prevClientIds.length !== clientIds.length) {
-            throw new exceptions.InvalidRequest(`Changing the client ids of an oidc provider is not supported.`);
-        }
-
-        for (const index in clientIds) {
-            if (prevClientIds[index] !== clientIds[index]) {
-                throw new exceptions.InvalidRequest(`Changing the client ids of an oidc provider is not supported.`);
-            }
-        }
-
-        await service
-            .updateOpenIDConnectProviderThumbprint({
-                ThumbprintList: model.thumbprintList,
+        for(const clientIdToRemove of clientIdsNeedToRemove) {
+            const request: RemoveClientIDFromOpenIDConnectProviderRequest = {
                 OpenIDConnectProviderArn: model.arn,
-            })
+                ClientID: clientIdToRemove
+            };
+            console.info({message: 'removing client id', ...request});
+            await service.removeClientIDFromOpenIDConnectProvider(request).promise();
+        }
+
+        for(const clientIdToAdd of clientIdsNeedToAdd) {
+            const request: AddClientIDToOpenIDConnectProviderRequest = {
+                OpenIDConnectProviderArn: model.arn,
+                ClientID: clientIdToAdd
+            };
+            console.info({message: 'adding client id', ...request});
+            await service.addClientIDToOpenIDConnectProvider(request).promise();
+        }
+
+        const updateThumbPrintsRequest: UpdateOpenIDConnectProviderThumbprintRequest = {
+            ThumbprintList: model.thumbprintList,
+            OpenIDConnectProviderArn: model.arn,
+
+        }
+        console.info({message: 'updating thumb prints', ...updateThumbPrintsRequest, previous: previousModel.thumbprintList});
+        await service
+            .updateOpenIDConnectProviderThumbprint(updateThumbPrintsRequest)
             .promise();
 
         return Promise.resolve(model);
@@ -72,7 +85,7 @@ class Resource extends BaseResource<ResourceModel> {
     }
 }
 
-const resource = new Resource(ResourceModel.TYPE_NAME, ResourceModel);
+export const resource = new Resource(ResourceModel.TYPE_NAME, ResourceModel);
 
 export const entrypoint = resource.entrypoint;
 
