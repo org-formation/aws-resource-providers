4a8f63855dca80fbb292785e5c5ff894fd51400f
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2020-12-09 15:57:27 +0100

    use jest circus runner

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ const base = require('./jest.config.base.js');
 module.exports = {
     ...base,
     setupFiles: ['<rootDir>/setup.js'],
-    // testRunner: 'jest-circus/runner',
+    testRunner: 'jest-circus/runner',
     projects: ['<rootDir>/**/jest.config.js'],
     coverageDirectory: '<rootDir>/coverage/',
 };
