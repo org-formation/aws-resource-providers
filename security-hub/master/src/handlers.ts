e953f8ffaf5d7a744bab4214ab9d56b61895aacf
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2021-01-22 20:46:19 +0100

    add delete handler for master resource

--- security-hub/master/src/handlers.ts
+++ security-hub/master/src/handlers.ts
@@ -1,6 +1,6 @@
 import { SecurityHub } from 'aws-sdk';
 import { commonAws, HandlerArgs } from 'aws-resource-providers-common';
-import { Action, BaseResource, handlerEvent, Logger } from 'cfn-rpdk';
+import { Action, BaseResource, exceptions, handlerEvent, Logger } from 'cfn-rpdk';
 
 import { ResourceModel } from './models';
 
@@ -19,7 +19,7 @@ async function acceptMaster(desiredModel: ResourceModel, logger: Logger, logging
     logger.log({ ...loggingContext, method: 'after listInvitations', invitations });
 
     for (const invitation of invitations.Invitations) {
-        if (invitation.AccountId === desiredModel.masterAccountId) {
+        if (invitation.AccountId === desiredModel.masterAccountId && invitation.MemberStatus === 'INVITED') {
             const acceptInvitationRequest: SecurityHub.AcceptInvitationRequest = { InvitationId: invitation.InvitationId, MasterId: desiredModel.masterAccountId };
 
             logger.log({ ...loggingContext, method: 'before acceptInvitation', acceptInvitationRequest });
@@ -37,7 +37,9 @@ class Resource extends BaseResource<ResourceModel> {
     public async create(action: Action, args: HandlerArgs<ResourceModel>, service: SecurityHub, model: ResourceModel): Promise<ResourceModel> {
         const { awsAccountId, clientRequestToken } = args.request;
         const loggingContext: LogContext = { handler: action, clientRequestToken: clientRequestToken, versionCode };
-
+        if (model.resourceId) {
+            throw new exceptions.InvalidRequest('Read only property [ResourceId] cannot be provided by the user.');
+        }
         args.logger.log({ ...loggingContext, message: 'begin', args });
         await acceptMaster(model, args.logger, loggingContext, service);
 
@@ -50,9 +52,14 @@ class Resource extends BaseResource<ResourceModel> {
     @handlerEvent(Action.Update)
     @commonAws({ service: SecurityHub, debug: true })
     public async update(action: Action, args: HandlerArgs<ResourceModel>, service: SecurityHub, model: ResourceModel): Promise<ResourceModel> {
-        const { clientRequestToken } = args.request;
+        const { clientRequestToken, logicalResourceIdentifier, previousResourceState } = args.request;
         const loggingContext: LogContext = { handler: action, clientRequestToken: clientRequestToken, versionCode };
-
+        if (!model.resourceId) {
+            throw new exceptions.NotFound(this.typeName, logicalResourceIdentifier);
+        } else if (model.resourceId !== previousResourceState.resourceId) {
+            args.logger.log(this.typeName, `[NEW ${model.resourceId}] [${logicalResourceIdentifier}] does not match identifier from saved resource [OLD ${previousResourceState.resourceId}].`);
+            throw new exceptions.NotUpdatable('Read only property [ResourceId] cannot be updated.');
+        }
         args.logger.log({ ...loggingContext, message: 'begin', args });
         await acceptMaster(model, args.logger, loggingContext, service);
 
@@ -62,23 +69,49 @@ class Resource extends BaseResource<ResourceModel> {
 
     @handlerEvent(Action.Delete)
     @commonAws({ service: SecurityHub, debug: true })
-    public async delete(action: Action, args: HandlerArgs<ResourceModel>): Promise<null> {
-        const { clientRequestToken } = args.request;
+    public async delete(action: Action, args: HandlerArgs<ResourceModel>, service: SecurityHub, model: ResourceModel): Promise<null> {
+        const { clientRequestToken, logicalResourceIdentifier } = args.request;
         const loggingContext: LogContext = { handler: action, clientRequestToken: clientRequestToken, versionCode };
+        if (!model.resourceId) {
+            throw new exceptions.NotFound(this.typeName, logicalResourceIdentifier);
+        }
 
-        args.logger.log({ ...loggingContext, message: 'noop', args });
+        try {
+            const deleteInvitationRequest: SecurityHub.DeleteInvitationsRequest = { AccountIds: [model.masterAccountId] };
+            args.logger.log({ ...loggingContext, method: 'before deleteInvitations', deleteInvitationRequest });
+            const acceptResponse = await service.deleteInvitations(deleteInvitationRequest).promise();
+            args.logger.log({ ...loggingContext, method: 'after deleteInvitations', acceptResponse });
+        } catch (err) {
+            if (err?.code === 'ResourceNotFoundException') {
+                throw new exceptions.NotFound(this.typeName, model.masterAccountId);
+            } else {
+                // Raise the original exception
+                throw err;
+            }
+        }
 
         return null;
     }
 
     @handlerEvent(Action.Read)
     @commonAws({ service: SecurityHub, debug: true })
-    public async read(action: Action, args: HandlerArgs<ResourceModel>, _service: SecurityHub, model: ResourceModel): Promise<ResourceModel> {
-        const { clientRequestToken } = args.request;
+    public async read(action: Action, args: HandlerArgs<ResourceModel>, service: SecurityHub, model: ResourceModel): Promise<ResourceModel> {
+        const { clientRequestToken, logicalResourceIdentifier } = args.request;
         const loggingContext: LogContext = { handler: action, clientRequestToken: clientRequestToken, versionCode };
+        if (!model.resourceId) {
+            throw new exceptions.NotFound(this.typeName, logicalResourceIdentifier);
+        }
 
-        args.logger.log({ ...loggingContext, message: 'noop', args });
+        args.logger.log({ ...loggingContext, message: 'begin', args });
+        const result = await service.listInvitations().promise();
+        const enabledInvitations = (result.Invitations || []).filter((value: SecurityHub.Invitation) => {
+            return value.MemberStatus === 'ENABLED';
+        });
+        if (!enabledInvitations.length) {
+            throw new exceptions.NotFound(this.typeName, model.masterAccountId);
+        }
 
+        args.logger.log({ ...loggingContext, message: 'done', result });
         return model;
     }
 }
