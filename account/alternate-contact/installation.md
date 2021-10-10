91f1e2473b7889efbdb282f49ca14823a1252d89
Olaf Conijn <olaf_conijn@hotmail.com>
2021-10-10 12:51:56 +0200

    feat: alternate contacts

---
+++ account/alternate-contact/installation.md
@@ -0,0 +1,46 @@
+# Community::Organizations::NoDefaultVPC
+
+## Installation using AWS CLI
+``` bash
+# first install the execution role
+aws cloudformation create-stack \
+  --template-url https://community-resource-provider-catalog.s3.amazonaws.com/community-account-alternatecontact-resource-role-0.1.0.yml \
+  --stack-name community-organizations-alternatecontact-resource-role \
+  --capabilities CAPABILITY_IAM
+
+aws cloudformation wait stack-create-complete \
+  --stack-name community-organizations-alternatecontact-resource-role
+
+# get the value of the ExecutionRoleArn Output
+aws cloudformation describe-stacks \
+  --stack-name community-organizations-alternatecontact-resource-role
+
+# register the cloudformation type
+aws cloudformation register-type \
+  --type-name Community::Organizations::NoDefaultVPC \
+  --type RESOURCE \
+  --schema-handler-package s3://community-resource-provider-catalog/community-account-alternatecontact-0.1.0.zip \
+  --execution-role <role-arn-from-output>
+
+aws cloudformation describe-type-registration --registration-token <registration-token>
+
+aws cloudformation set-type-default-version \
+  --version-id <version-id> \
+  --type-name Community::Account::AlternateContact \
+  --type RESOURCE
+```
+
+## Installation using org-formation task
+For more information on AWS Organization Formation, see: https://github.com/org-formation/org-formation-cli
+
+``` yaml
+CommunityAlternateContactsRP:
+  Type: register-type
+  SchemaHandlerPackage: s3://community-resource-provider-catalog/community-account-alternatecontact-0.1.0.zip
+  ResourceType: 'Community::Account::AlternateContact'
+  MaxConcurrentTasks: 10
+  OrganizationBinding:
+    IncludeMasterAccount: true
+    Account: '*'
+    Region: us-east-1
+```
---
+++ account/alternate-contact/src/handlers.ts
@@ -0,0 +1,59 @@
+import { Action, BaseResource, handlerEvent } from '@amazon-web-services-cloudformation/cloudformation-cli-typescript-lib';
+import { commonAws } from 'aws-resource-providers-common';
+import { mainModule } from 'process';
+import { ResourceModel } from './models';
+
+class Resource extends BaseResource<ResourceModel> {
+    @handlerEvent(Action.Create)
+    @commonAws({ serviceName: 'Account', debug: true })
+    public async create(action: Action, args: never, service: AWS.Account, model: ResourceModel): Promise<ResourceModel> {
+        const _ = await service
+            .putAlternateContact({
+                AlternateContactType: model.alternateContactType,
+                AccountId: model.accountId,
+                Name: model.name,
+                PhoneNumber: model.phoneNumber,
+                Title: model.title,
+                EmailAddress: model.emailAddress,
+            })
+            .promise();
+
+        return Promise.resolve(model);
+    }
+    @handlerEvent(Action.Update)
+    @commonAws({ serviceName: 'Account', debug: true })
+    public async update(action: Action, args: never, service: AWS.Account, model: ResourceModel): Promise<ResourceModel> {
+        const _ = await service
+            .putAlternateContact({
+                AlternateContactType: model.alternateContactType,
+                AccountId: model.accountId,
+                Name: model.name,
+                PhoneNumber: model.phoneNumber,
+                Title: model.title,
+                EmailAddress: model.emailAddress,
+            })
+            .promise();
+
+        return Promise.resolve(model);
+    }
+    @handlerEvent(Action.Update)
+    @commonAws({ serviceName: 'Account', debug: true })
+    public async delete(action: Action, args: never, service: AWS.Account, model: ResourceModel): Promise<ResourceModel> {
+        const _ = await service
+            .deleteAlternateContact({
+                AlternateContactType: model.alternateContactType,
+                AccountId: model.accountId,
+            })
+            .promise();
+
+        return Promise.resolve(model);
+    }
+}
+
+export const resource = new Resource(ResourceModel.TYPE_NAME, ResourceModel);
+
+// Entrypoint for production usage after registered in CloudFormation
+export const entrypoint = resource.entrypoint;
+
+// Entrypoint used for local testing
+export const testEntrypoint = resource.testEntrypoint;
