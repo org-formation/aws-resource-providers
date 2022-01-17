ff9516e35b74f0e085cc9cdef79f7f0f30f58842
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2022-01-17 23:18:36 +0100

    chore: use same version of cfn rpdk

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @typedef {import('ts-jest/dist/types')} */
 /** @type {import('@jest/types').Config.InitialOptions} */
+
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
     preset: 'ts-jest',
@@ -17,4 +22,5 @@ module.exports = {
     coverageProvider: 'v8',
     coverageReporters: ['json', 'lcov', 'text'],
     coveragePathIgnorePatterns: ['node_modules/', '__tests__/data/', 'src/models.ts'],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 };
