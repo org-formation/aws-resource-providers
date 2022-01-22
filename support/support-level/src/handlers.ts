8f4afe2c319c4cb64430e844fc72f9c6899f1f10
Olaf Conijn <olaf_conijn@hotmail.com>
2022-01-22 11:55:32 +0100

    feat(support): add support level (#47)
    * feat(suppor-support-level): added initial version
    
    * fix(support-support-level): fixed tsconfig and .npmrc
    
    * chore: linting
    
    * feat: added check on changing support-level in update
    
    * chore: linting
    
    * feat(support-support-level): added DisableSupportCaseCreation attribute
    
    * bumped version
    
    * ci: re-trigger build
    
    * chore: update rpdk dependencies
    
    * chore: add unit tests
    
    Co-authored-by: Pat Myron <pat.myron@stedi.com>
    Co-authored-by: Eduardo Rodrigues <eduardomourar@users.noreply.github.com>

---
+++ support/support-level/src/handlers.ts
@@ -0,0 +1,83 @@
+import { Organizations, Support } from 'aws-sdk';
+import { commonAws, HandlerArgs } from 'aws-resource-providers-common';
+import { Action, BaseResource, exceptions, handlerEvent } from '@amazon-web-services-cloudformation/cloudformation-cli-typescript-lib';
+import { ResourceModel } from './models';
+import { CreateCaseRequest } from 'aws-sdk/clients/support';
+
+export async function createSupportCase(model: ResourceModel, service: Support): Promise<void> {
+    if (model.disableSupportCaseCreation) {
+        return;
+    }
+
+    const createCaseRequest: CreateCaseRequest = {
+        subject: `Enable ${model.supportLevel} support for account: ${model.accountId}`,
+        communicationBody: `Hi AWS,
+        Please enable ${model.supportLevel} on account ${model.accountId}.
+        This case was created automatically - please resolve when done.
+
+        Thank you!
+            `,
+        serviceCode: 'customer-account',
+        categoryCode: 'other-account-issues',
+        severityCode: 'low',
+        issueType: 'customer-service',
+        ccEmailAddresses: model.cCEmailAddresses,
+    };
+
+    await service.createCase(createCaseRequest).promise();
+}
+
+async function checkContextIsOrganizationMasterAccount(args: HandlerArgs<ResourceModel, Record<string, any>>): Promise<void> {
+    try {
+        const organizationsClient = args.session.client('Organizations', { region: 'us-east-1' }) as Organizations;
+        await organizationsClient.describeOrganization().promise();
+    } catch (err) {
+        throw new exceptions.InvalidRequest(`Account does not seem to be the master account of an AWS Organization.\n${err}`);
+    }
+}
+
+class Resource extends BaseResource<ResourceModel> {
+    @handlerEvent(Action.Create)
+    @commonAws({ serviceName: 'Support', debug: true })
+    public async create(action: Action, args: HandlerArgs<ResourceModel>, service: Support, model: ResourceModel): Promise<ResourceModel> {
+        const targetAccountId = args.request.awsAccountId;
+
+        await checkContextIsOrganizationMasterAccount(args);
+        await createSupportCase(model, service);
+
+        model.arn = `arn:community:organizations::${targetAccountId}:support/support-level-${model.accountId}`;
+
+        return model;
+    }
+
+    @handlerEvent(Action.Update)
+    @commonAws({ serviceName: 'Support', debug: true })
+    public async update(action: Action, args: HandlerArgs<ResourceModel>, service: Support, model: ResourceModel): Promise<ResourceModel> {
+        if (args.request.previousResourceState.supportLevel !== args.request.desiredResourceState.supportLevel) {
+            await createSupportCase(model, service);
+        }
+
+        return model;
+    }
+
+    @handlerEvent(Action.Delete)
+    @commonAws({ serviceName: 'Support', debug: true })
+    public async delete(): Promise<null> {
+        return null;
+    }
+
+    @handlerEvent(Action.Read)
+    @commonAws({ serviceName: 'Support', debug: true })
+    public async read(action: Action, args: HandlerArgs<ResourceModel>, service: Support, model: ResourceModel): Promise<ResourceModel> {
+        const read = new ResourceModel({
+            Arn: model.arn,
+        });
+        return Promise.resolve(read);
+    }
+}
+
+export const resource = new Resource(ResourceModel.TYPE_NAME, ResourceModel);
+
+export const entrypoint = resource.entrypoint;
+
+export const testEntrypoint = resource.testEntrypoint;
